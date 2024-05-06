2644f0ad869a6153670bc5dd935145a0befc6bc8
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 05:16:58 -0700

    Now names have been changed

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -2,8 +2,8 @@
 
 echo "The first shell-script"
 
-person1=ramesh
-person2=suresh
+person1=shankar
+person2=sai
 
 echo  $person2 :"Hello $person1 how are you bunch of issues were pending on deployement, could you please solve them and close the ticket"
 echo  $person1 :"Hey, sure $person2 I need to terminate my unused instances in aws , So need to write one shell-script"
