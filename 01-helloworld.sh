38c09a194721ca863db570dc6a083c954157410d
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 04:55:10 -0700

    Did some changes

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "The first shell-script"
+
+echo "Hello ramesh how are you bunch of issues were pending on deployement, could you please solve them and close the ticket"
+echo " Hey, sure suresh I need to terminate my unused instances in aws , So need to write one shell-script"
+echo " well , I heard that you working on multiple project is it true Oh man how can you handle such pressure can you please let me know"
+echo " it not so tough , Iam just working another in freelancing meanwhile Iam working in office"
