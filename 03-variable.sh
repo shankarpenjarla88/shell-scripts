e522e6ab325342c783f03c8ff40651bca2dbb414
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 05:57:21 -0700

    giving values in commandline

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "The Thried shell-script"
+
+person1=$1
+person2=$2
+
+echo  $person2 :"Hello $person1 how are you bunch of issues were pending on deployement, could you please solve them and close the ticket"
+echo  $person1 :"Hey, sure $person2 I need to terminate my unused instances in aws , So need to write one shell-script"
+echo  $person2 :"well , I heard that you working on multiple project is it true Oh man how can you handle such pressure can you please let me know"
+echo  $person1 :"it not so tough , Iam just working another in freelancing meanwhile Iam working in office"
