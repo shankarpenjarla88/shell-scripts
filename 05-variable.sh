ab4cd7ef7e75e11c429d1522b85ce0dcc9982e7a
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 06:23:33 -0700

    conneting to database via username and password

---
+++ 05-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username"
+
+read $username
+
+echo "The username you entered is: $username"
+
+echo "Please enter your password"
+
+read $password
+
+echo "The password you entered is: $password"
