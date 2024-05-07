d304a245f73e9838b3ae17ed5cd51581cdaba790
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 21:48:50 -0700

    Mysqlinstall

---
+++ 10-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#installing mysql 
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then 
+  echo "ERROR:PLEASE LOGIN TO ADMIN ACCESS"
+  exit 1 
+else
+  echo "NOW YOU ARE IN ROOT ACCESS,INSTALLATION IN PROCESS"
+
+  yum install mysql -y
