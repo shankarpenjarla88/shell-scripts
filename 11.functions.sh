4954e048839641d75d6ba959ccf8c063f4ec1f67
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-06 22:58:18 -0700

    Functions implimentation

---
+++ 11.functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+userid=$( id -u )
+
+validate() {
+    if [ $1 -ne 0 ]
+    then 
+      echo "INSTALLATION IS FAILED"
+    else
+      echo "INSTALLATION IS SUCCESS"
+    fi
+}
+
+if [ $userid -ne 0 ]
+then 
+  echo "ERROR:PLEASE LOGIN TO ADMIN ACCESS"
+  exit 1
+else
+  echo "NOW YOU ARE IN THE ROOT ACCESS"
+fi
+
+yum install git -y 
+validate $?
+
+yum install mysql -y 
+validate $?
+
+yum install postfix -y 
+validate $?
+
+
