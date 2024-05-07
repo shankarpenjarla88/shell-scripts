8f5846459b609199ca1e6afffd9693b89978811a
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-07 03:49:59 -0700

    loopwithmultiplepackages

---
+++ 15-pacloop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+validate() {
+    if [$1 -ne 0 ]
+    then
+      echo -e " $2 $R INSTALLATION IS FAILED $N"
+      exit 1
+    else
+      echo -e " $2 $G INSTALLATION IS SUCCESS $N"
+    fi
+
+}
+
+
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+  echo "ERROR:PLEASE LOGIN WITH ROOT ACCESS"
+  exit 1
+else
+  echo "NOW YOU ARE IN ROOT ACCESS<GOOD TO GO WITH INSTALLATION"
+fi
+
+for i in $@
+do
+   yum install $i -y 
+  validate $? $@
+done
+
+
