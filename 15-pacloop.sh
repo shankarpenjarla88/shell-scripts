477e7e3c6356a909c4f75b010929767bd3af1abf
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-07 04:05:41 -0700

    loopswithpackages

--- 15-pacloop.sh
+++ 15-pacloop.sh
@@ -5,8 +5,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+Date=$( date + %f )
+scriptname=$0
+logfile1= /tmp/ $scriptname-$Date.log
+
 validate() {
-    if [$1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
       echo -e " $2 $R INSTALLATION IS FAILED $N"
       exit 1
@@ -30,8 +34,8 @@ fi
 
 for i in $@
 do
-   yum install $i -y 
-  validate $? $@
+   yum install $i -y  &>>$logfile1
+   validate $? "$@ INSTALLATION IS" 
 done
 
 
