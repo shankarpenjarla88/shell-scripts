59442662dbb88cedf76b832da6b3ef04a196a5d0
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-07 22:11:42 -0700

    Logsdeletions

--- 16-logdel.sh
+++ 16-logdel.sh
@@ -14,3 +14,15 @@ files_to_delete=$( $applogdir -name "*.log" -type f -mtime +14)
 
 echo "$files_to_delete"
 
+while read line 
+
+do
+
+#we wil write all the statements here
+
+echo "Deleting $line" &>$logfile
+rm -rf $line
+
+
+done < $file_to_delete
+
