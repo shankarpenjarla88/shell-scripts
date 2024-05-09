07fbf16ae9119def1788377224a3da64329bd19a
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-08 21:33:18 -0700

    logfilesdeletion

--- 17-del_logs.sh
+++ 17-del_logs.sh
@@ -14,7 +14,7 @@ files_del=$(find $log_dir -name "*.log" -type f -mtime +14)
 
 echo "This are the deleting files:$files_del"
 
-while read Line 
+while read line 
 do
   echo "Deleting the log fles which are more than two weeks:$line" &>>$log_file
   rm -rf $line 
