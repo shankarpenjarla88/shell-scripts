ae4f966d3a3480e2ea158a6bb84fd121adbbaf73
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-09 02:15:08 -0700

    DiskUsage

---
+++ 18-diskusage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOG_FOL=/tmp/
+LOG_FILE=$LOG_FOL/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|filesystem')
+DISK_USAGE_THRESHOLD=1
+
+#IFS=Internal Filed saperator is space.
+
+while IFS= read line
+do
+  echo "output:$line"
+
+done >>> $DISK_USAGE
+
+
