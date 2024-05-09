84dc08ef2825e555a6157e0815c949881119e924
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-08 21:36:27 -0700

    logfiledel

--- 17-del_logs.sh
+++ 17-del_logs.sh
@@ -8,7 +8,7 @@ script_name=$0
 
 log_fol=/root/shell-script-logs
 
-log_file=$log_fol/$script_name/$date.log
+log_file=$log_fol/$script_name-$date.log
 
 files_del=$(find $log_dir -name "*.log" -type f -mtime +14)
 
@@ -16,6 +16,6 @@ echo "This are the deleting files:$files_del"
 
 while read line 
 do
-  echo "Deleting the log fles which are more than two weeks:$line" &>>$log_file
+  echo "Deleting $line" &>> $log_file
   rm -rf $line 
 done <<< $files_del
